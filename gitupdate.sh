281c5ad6db9e82138b2a860a4e7f8696452386f7
Grady Neely <grady.neely@icloud.com>
2017-11-16 16:19:09 -0600

    cleaning up

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+RSYNC="/usr/bin/rsync -avrz --progress --delete"
+MOUNTDIR=/Volumes/config
 git add .
 git status
 echo -n "Enter the Description for the Change: " [Minor Update]
@@ -7,4 +9,13 @@ read CHANGE_MSG
 git commit -m "${CHANGE_MSG}"
 git push origin master
 
+if [ ! -d $MOUNTDIR ];
+    mount -t smbfs "//hassio/config"
+    $RSYNC /Users/gneely/Documents/hassio/homeassistant-config/ /Volumes/config/
+    diskutil unmount /Volumes/config
+else
+    $RSYNC /Users/gneely/Documents/hassio/homeassistant-config/ /Volumes/config/
+    diskutil unmount /Volumes/config
+fi
+    
 exit
