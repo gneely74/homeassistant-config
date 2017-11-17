024c5672033c42c4f9f0cd4d73747779329a85b4
Grady Neely <grady.neely@icloud.com>
2017-11-17 10:25:17 -0600

    .

--- gitupdate.sh
+++ gitupdate.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-RSYNC="/usr/bin/rsync -avrz --progress --exclude='.git/' -e 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'"
 git add .
 git status
 if [[ `git status --porcelain` ]]; then
@@ -8,7 +7,7 @@ if [[ `git status --porcelain` ]]; then
     read CHANGE_MSG
     git commit -m "${CHANGE_MSG}"
     git push origin master
-    $RSYNC /Users/gneely/Documents/hassio/homeassistant-config/ root@hassio.local:/config/
+    /usr/bin/rsync -avrz --progress --exclude='.git/' -e 'ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null' /Users/gneely/Documents/hassio/homeassistant-config/ root@hassio.local:/config/
     ssh -o StrictHostKeyChecking=no root@hassio.local hassio homeassistant restart  
 else
   echo ""
