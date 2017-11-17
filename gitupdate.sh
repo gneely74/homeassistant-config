b92c78425bf1079e88a244e4101db6e3b596be4e
Grady Neely <grady.neely@icloud.com>
2017-11-17 09:13:17 -0600

    changed the giyupdate script

--- gitupdate.sh
+++ gitupdate.sh
@@ -4,22 +4,25 @@ RSYNC="/usr/bin/rsync -avrz --progress --exclude='.git/'"
 MOUNTDIR=/Volumes/config/
 git add .
 git status
-echo -n "Enter the Description for the Change: " [Minor Update]
-read CHANGE_MSG
-git commit -m "${CHANGE_MSG}"
-git push origin master
+if [[ `git status --porcelain` ]]; then
+    echo -n "Enter the Description for the Change: " [Minor Update]
+    read CHANGE_MSG
+    git commit -m "${CHANGE_MSG}"
+    git push origin master
 
-if [ ! -d $MOUNTDIR ];
-    then
-        open 'smb://Guest:password@hassio/config'
-        sleep 5
-        $RSYNC /Users/gneely/Documents/hassio/homeassistant-config/ $MOUNTDIR
-        diskutil unmount $MOUNTDIR
-    else
-        $RSYNC /Users/gneely/Documents/hassio/homeassistant-config/ $MOUNTDIR
-        diskutil unmount $MOUNTDIR
+    if [ ! -d $MOUNTDIR ];
+        then
+            open 'smb://Guest:password@hassio/config'
+            sleep 5
+            $RSYNC /Users/gneely/Documents/hassio/homeassistant-config/ $MOUNTDIR
+            diskutil unmount $MOUNTDIR
+        else
+            $RSYNC /Users/gneely/Documents/hassio/homeassistant-config/ $MOUNTDIR
+            diskutil unmount $MOUNTDIR
+    fi
+    ssh -o StrictHostKeyChecking=no root@hassio.local hassio homeassistant restart   
+else
+  echo "No changes"
 fi
-
-curl -X GET -H "x-ha-access: Gr8eandK8" -H "Content-Type: application/json" http://hassio.local:8123/api/homeassistant/restart
-ssh root@hassio.local hassio homeassistant restart    
+ 
 exit
