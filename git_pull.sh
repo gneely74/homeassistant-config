494ae5d7e6ffef600feb13a503812d76ebd03fa1
Grady Neely <grady.neely@adtran.com>
2017-11-21 14:16:04 -0600

    added /etc/periodic/git_pull.sh

---
+++ git_pull.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+dir="/config"
+lock=/tmp/update.lock
+msglog="/var/log/update.log"
+
+log()
+{
+        echo "$(date) ${1:-missing}" >> $msglog
+}
+
+if [ -f $lock ]; then
+        log "Already run, exiting..."
+else
+        > $lock
+        git -C $dir remote update &> /dev/null
+        checkgit=`git -C $dir status`
+        if [[ ! "$checkgit" =~ "Your branch is up-to-date" ]]; then
+                log "-------------- Update ---------------"
+                git -C $dir pull &>> $msglog
+                log "-------------------------------------"
+                /usr/bin/hassio homeassistant restart
+        fi
+        rm $lock
+
+fi
+exit 0
