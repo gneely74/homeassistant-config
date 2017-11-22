133ad73d224f4aea82d85ce96bde29d4be4e3b71
Grady Neely <grady.neely@adtran.com>
2017-11-22 07:06:30 -0600

    updated light automation for fronyt

--- gitupdate.sh
+++ gitupdate.sh
@@ -7,7 +7,6 @@ if [[ `git status --porcelain` ]]; then
     read CHANGE_MSG
     git commit -m "${CHANGE_MSG}"
     git push origin master
-    /usr/bin/rsync -avrz --progress --exclude='.git/' -e 'ssh -p 9922 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null' /Users/gneely/Documents/hassio/homeassistant-config/ root@home.theneelyfamily.net:/config/
     ssh -p 9922 -o StrictHostKeyChecking=no root@home.theneelyfamily.net /etc/periodic/15min/git_pull.sh  
 else
   echo ""
