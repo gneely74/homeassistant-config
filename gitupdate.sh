5448c94539d64a849bc59548b87316ce5c4ab354
Grady Neely <grady.neely@adtran.com>
2017-12-07 10:40:27 -0600

    -

--- gitupdate.sh
+++ gitupdate.sh
@@ -7,7 +7,7 @@ if [[ `git status --porcelain` ]]; then
     read CHANGE_MSG
     git commit -m "${CHANGE_MSG}"
     git push origin master
-    ssh -p 9922 -o StrictHostKeyChecking=no root@home.theneelyfamily.net /etc/periodic/15min/git_pull.sh  
+    ssh -p 9922 -o StrictHostKeyChecking=no root@home.theneelyfamily.net /config/git_pull.sh  
 else
   echo ""
 fi
