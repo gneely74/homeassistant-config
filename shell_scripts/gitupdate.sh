68a5b5783624818dad4cb5c0a72c8198640387eb
Grady Neely <grady.neely@adtran.com>
2017-12-18 08:31:20 -0600

    -

--- shell_scripts/gitupdate.sh
+++ shell_scripts/gitupdate.sh
@@ -7,8 +7,8 @@ if [[ `git status --porcelain` ]]; then
     read CHANGE_MSG
     git commit -m "${CHANGE_MSG}"
     git push origin master
-    scp -P 9922 -o StrictHostKeyChecking=no secrets.yaml root@home.theneelyfamily.net:/config/
-    ssh -p 9922 -o StrictHostKeyChecking=no root@home.theneelyfamily.net /config/shell_scripts/git_pull.sh  
+    scp -P 9922 -o StrictHostKeyChecking=no ../secrets.yaml root@home.theneelyfamily.net:/config/
+#     ssh -p 9922 -o StrictHostKeyChecking=no root@home.theneelyfamily.net /config/shell_scripts/git_pull.sh  
 else
   echo ""
 fi
